85418e90a32997332c35ea226dbec97328b8f5ba
Richard Chukwu <richinex@gmail.com>
2023-02-15 18:27:44 +0100

    recipe data model

--- main.go
+++ main.go
@@ -6,3 +6,11 @@ func main() {
 	router := gin.Default()
 	router.Run()
 }
+
+type Recipe struct {
+	Name         string   `json:"name"`
+	Tags         []string `json:"tags"`
+	Ingredients  []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt  string   `json:"published_at"`
+}
